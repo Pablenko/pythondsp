c79d25caf21aa26248b225759cae8dc2491e7aef
Pawel Przybylski <pawel.przybylski91@gmail.com>
2018-07-27 12:38:00 +0200

    Added dft and polar coords.

--- pythondsp.py
+++ pythondsp.py
@@ -22,10 +22,45 @@ def generate_frequencies(signal_params, generator):
     return t, ret
 
 
+def dft(input):
+    signal_length = len(input)
+    dft_length = signal_length / 2
+
+    re_signal = np.zeros(dft_length)
+    im_signal = np.zeros(dft_length)
+
+    for k in xrange(dft_length):
+        for n in xrange(signal_length):
+            re_signal[k] += input[n] * np.cos(2*np.pi*k*n / signal_length)
+            im_signal[k] += -input[n] * np.sin(2*np.pi*k*n / signal_length)
+
+    return re_signal, im_signal
+
+
+def polar_coords(re, im):
+    def magnitude():
+        for k in xrange(len(re)):
+            yield np.sqrt(re[k]*re[k] + im[k]*im[k])
+
+    def phase():
+        for k in xrange(len(im)):
+            yield np.arctan(im[k] / re[k])
+
+    return [m for m in magnitude()], [p for p in phase()]
+
+
 def main():
     signal_params = [signal_param(1, 1), signal_param(1, 5)]
     x, y = generate_frequencies(signal_params, generate_sin)
-    plt.plot(x, y)
+
+    re, im = dft(y)
+    x = np.arange(0, len(y)/2)
+    mag, phase = polar_coords(re, im)
+
+    plt.subplot(211)
+    plt.plot(x, mag)
+    plt.subplot(212)
+    plt.plot(x, phase)
     plt.show()
 
 
